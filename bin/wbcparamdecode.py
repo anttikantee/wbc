88fe7760047bac894c19e2d8e6601644085297ed
Antti Kantee <pooka@iki.fi>
2018-11-27 19:12:25 +0000

    add wbcparamdecode
    takes a string of short param names as an argument and returns long ones.
    it's not too difficult to figure out the long names from the short names
    even without this utility, but when we can make it explicit with a few
    lines of code, why not.

---
+++ bin/wbcparamdecode.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2018 Antti Kantee <pooka@iki.fi>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+from WBC.Sysparams import decodeparamshorts
+from WBC.Utils import PilotError
+
+import getopt
+import sys
+
+def usage():
+	sys.stderr.write('usage: ' + sys.argv[0] + ' paramstring\n')
+	sys.exit(1)
+
+if __name__ == '__main__':
+	opts, args = getopt.getopt(sys.argv[1:], '')
+
+	if len(args) != 1:
+		usage()
+
+	try:
+		res = decodeparamshorts(args[0])
+		longest = max([len(x[0]) for x in res])
+		fmtstr = '{:' + str(longest) + '} : {:}'
+		for x in res:
+			print fmtstr.format(x[0], x[1])
+	except PilotError, e:
+		print e
+		sys.exit(1)
+	sys.exit(0)
