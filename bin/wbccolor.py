5bf3e8fdc7ff7a1cd96da1803f909d6a097f32b9
Antti Kantee <pooka@iki.fi>
2018-12-08 20:10:06 +0000

    add wbccolor
    takes in a color in EBC, SRM or Lovibond, and prints all three

---
+++ bin/wbccolor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2018 Antti Kantee <pooka@iki.fi>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+from WBC.WBC import Recipe
+from WBC import Parse
+from WBC.Units import Color
+from WBC.Utils import PilotError
+
+import getopt
+import sys
+
+def usage():
+	sys.stderr.write('usage: ' + sys.argv[0] + ' color\n')
+	sys.exit(1)
+
+if __name__ == '__main__':
+	opts, args = getopt.getopt(sys.argv[1:], '')
+
+	if len(args) != 1:
+		usage()
+
+	try:
+		c = Parse.color(args[0])
+	except PilotError, e:
+		print e
+		sys.exit(1)
+
+	print '{:.1f} EBC {:10.1f} SRM {:10.1f} L'.format(c.valueas(Color.EBC),
+	    c.valueas(Color.SRM), c.valueas(Color.LOVIBOND))
+	sys.exit(0)
