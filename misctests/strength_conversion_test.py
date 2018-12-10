6a43c804e1704c1389b1461c7126fde02b67ec6f
Antti Kantee <pooka@iki.fi>
2018-12-10 23:02:32 +0000

    misctests: print max "drift" in plato<->sg

--- misctests/strength_conversion_test.py
+++ misctests/strength_conversion_test.py
@@ -4,19 +4,29 @@ from WBC.WBC import Recipe
 from WBC.Units import Strength
 
 def testme(vals, fun, ifun):
+	rv = []
 	for x in vals:
 		v = fun(x)
 		iv = ifun(v)
 		diff = abs(x - iv)
-		print x, v, iv, diff
+		rv.append((x, v, iv, diff))
+	return rv
+
+def prt(v):
+	for x in v:
+		print x[0], x[1], x[2], x[3]
+	sv = sorted(v, key=lambda x: x[3], reverse=True)
+	print 'max diff at', sv[0][0], '(' + str(sv[0][3]) + ')'
 
 if __name__ == '__main__':
-	r = [x / 1000.0 for x in range(1001, 1100)]
+	r = [x / 1000.0 for x in range(1001, 1140)]
 	print 'SG to Plato'
 	v = testme(r, Strength.sg_to_plato, Strength.plato_to_sg)
+	prt(v)
 
 	print
 
-	r = [x / 10.0 for x in range(0, 250, 2)]
+	r = [x / 10.0 for x in range(0, 300, 2)]
 	print 'Plato to SG'
 	v = testme(r, Strength.plato_to_sg, Strength.sg_to_plato)
+	prt(v)
