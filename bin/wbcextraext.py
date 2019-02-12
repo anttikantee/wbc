fb0d964d099d895b4e8ba11835fc2e0462981ccf
Antti Kantee <pooka@iki.fi>
2019-02-12 19:40:10 +0000

    add wbcextraext for calculations on extract into existing wort
    The tool is useful for calculations based on values that are not
    entirely known at recipe formulation time (exact strength, volume,
    attenuation, ...), and can be used for more precise insight on
    final strength, abv, predicted gravity after the addition is
    fully fermented, etc.
    
    The tool takes original strength, volume, and amount of extract
    to be added.  The amount of extract can be specified as:
      * mass (useful when adding pure sugar or LME)
      * mass @ extract % by mass (useful e.g. for honey or LME)
      * volume @ strength (useful when adding a wort or starter)
    
    Optionally, using the -f parameter you can supply the final strength
    of the original wort, and the tool will calculate the aggregate final
    strength.  You can also supply the expected real attenuation of the
    addition.
    
    So, for example, if you have 5gal of 1.060 wort which fermented down
    to 1.015, and you add a pound of honey (75% extract, of which 95% is
    fermentable) to it, you can model that by:
    
    	wbcextraext.py -f 1.015,95% 1.060 5gal 1lb@75%
    
    And get the following result:
    
    =====
    Strength, Original          :        14.7°P         1.060
    Strength, Aggregate         :        16.1°P         1.066
    
    Extract, Original           :       2.96 kg      6 1/2 lb
    Extract, Added              :         340 g      12.00 oz
    Extract, Aggregate          :       3.30 kg      7 1/4 lb
    
    Water, Original             :      17.11 kg   37 11/16 lb
    Water, New                  :      17.22 kg   37 15/16 lb
    
    Volume, Original            :         18.9l        5.0gal
    Volume, Aggregate           :         19.3l        5.1gal
    
    Final Strength (95% add RA) :         3.4°P         1.013
    =====
    
    And then, if you like, you can use wbcabv to figure out the
    honey-included alcohol content (which is 5.9% without the
    honey and 7.0% with the honey, so not a trivial difference).

---
+++ bin/wbcextraext.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+#
+# Copyright (c) 2019 Antti Kantee <pooka@iki.fi>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+from WBC.WBC import Recipe
+from WBC.Units import Mass, _Mass, Strength, _Strength, Volume, _Volume
+from WBC import Parse
+from WBC.Utils import PilotError
+
+import getopt
+import os
+import sys
+
+def usage():
+	sys.stderr.write('usage: ' + os.path.basename(sys.argv[0])
+	    + ' [-f final_strength[,addition_RA%]]\n'
+	    + '\tstrength vol mass[@extract%]|vol@strength\n')
+	sys.exit(1)
+
+if __name__ == '__main__':
+	opts, args = getopt.getopt(sys.argv[1:], 'f:')
+
+	if len(args) != 3:
+		usage()
+
+	s_fin = ra_arg = None
+	for o, a in opts:
+		if o == '-f':
+			if ',' in a:
+				s_fin, ra_arg = Parse.split(a, ',',
+				    Parse.strength, Parse.percent)
+				if ra_arg < 0.0 or ra_arg > 100.0:
+					raise PilotError('need 0 < RA <= 100')
+			else:
+				s_fin = Parse.strength(a)
+		elif o == '-h':
+			usage()
+
+	s_orig = Parse.strength(args[0])
+	vol_orig = Parse.volume(args[1])
+
+	if '@' in args[2]:
+		if '%' in args[2]:
+			mass_add, percent = Parse.split(args[2], '@',
+			    Parse.mass, Parse.percent)
+			ext_add = _Mass(mass_add * percent/100.0)
+		else:
+			vol_add, s_add = Parse.split(args[2], '@',
+			    Parse.volume, Parse.strength)
+			mass_add = _Mass(s_add.valueas(s_add.SG) * vol_add)
+			ext_add = _Mass(mass_add * s_add/100.0)
+	else:
+		ext_add = mass_add = Parse.mass(args[2])
+
+	mass_orig = _Mass(s_orig.valueas(s_orig.SG) * vol_orig)
+	ext_orig = _Mass(mass_orig * s_orig/100.0)
+
+	ext_new = _Mass(ext_orig + ext_add)
+	mass_new = _Mass(mass_orig + mass_add)
+
+	s_new = _Strength(100.0*ext_new / mass_new)
+	vol_new = _Volume(mass_new / s_new.valueas(s_new.SG))
+
+	water_orig = _Mass(mass_orig - ext_orig)
+	water_new = _Mass(mass_new - ext_new)
+
+	if s_fin is not None:
+		r = s_orig.attenuate_bystrength(s_fin)
+		ra = r['ra']
+		if ra_arg is None:
+			ra_add = 100.0
+			aa = r['aa']
+		else:
+			ra_add = ra_arg
+			aa = None
+
+		# calculate aggregate RA, which is supplied RA for
+		# the original plus the given RA (if RA was not given,
+		# we'll calculate both 100% RA and [implicitly] given AA)
+		wantedra = 100*(ext_orig*ra/100.0 + ext_add*ra_add/100.0) \
+		    / (ext_orig + ext_add)
+		s_guess = _Strength(s_new/2)
+
+		# considering the formula for real extract (WBC/Units::Strength)
+		# I'm not too keen on solving real attenuation analytically.
+		# takes usually <=5 loops.
+		while True:
+			r = s_new.attenuate_bystrength(s_guess)
+			ra_delta = r['ra'] - wantedra
+			if abs(ra_delta) < 0.01:
+				break
+
+			# adjust by the fraction of the range that
+			# we're off by.  we don't hit it on the first tries
+			# due to the non-linear nature, but we'll get close(r).
+			s_guess = _Strength(s_guess + s_new*ra_delta/100.0)
+
+		s_ra = s_guess
+		if aa is not None:
+			s_aa = s_new.attenuate_bypercent(aa)['ae']
+
+	def printline2(fname, value1, value2):
+		print('{:28}:{:>14}{:>14}'.format(fname, value1, value2))
+
+	printline2('Strength, Original',
+	    s_orig.stras(Strength.PLATO), s_orig.stras(Strength.SG))
+	printline2('Strength, Aggregate',
+	    s_new.stras(Strength.PLATO), s_new.stras(Strength.SG))
+
+	print()
+	printline2('Extract, Original',
+	    ext_orig.stras_system('metric'), ext_orig.stras_system('us'))
+	printline2('Extract, Added',
+	    ext_add.stras_system('metric'), ext_add.stras_system('us'))
+	printline2('Extract, Aggregate',
+	    ext_new.stras_system('metric'), ext_new.stras_system('us'))
+
+	print()
+	printline2('Water, Original',
+	    water_orig.stras_system('metric'), water_orig.stras_system('us'))
+	printline2('Water, New',
+	    water_new.stras_system('metric'), water_new.stras_system('us'))
+
+	print()
+	printline2('Volume, Original',
+	    vol_orig.stras_system('metric'), vol_orig.stras_system('us'))
+	printline2('Volume, Aggregate',
+	    vol_new.stras_system('metric'), vol_new.stras_system('us'))
+
+	if s_fin is not None:
+		print()
+		printline2('Final Strength (' + str(int(ra_add)) + '% add RA)',
+		    s_ra.stras(Strength.PLATO), s_ra.stras(Strength.SG))
+		if aa is not None:
+			printline2('Final Strength (' + str(int(aa)) + '% AA)',
+			    s_aa.stras(Strength.PLATO), s_aa.stras(Strength.SG))
+
+	sys.exit(0)
