a142465d58f6d3dd8f12d07b5c5354dc166fbe40
Antti Kantee <pooka@iki.fi>
2021-04-29 22:36:07 +0000

    sysparams: checkpoint inline documentation work
    The parameters in the code now contain a description of
    what they're supposed to do.  Running WBC/sysparams.py
    dumps the documentation.
    
    Finishing the job requires some parameter rework.

--- WBC/sysparams.py
+++ WBC/sysparams.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 #
+#-
 # Copyright (c) 2018 Antti Kantee <pooka@iki.fi>
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -14,6 +16,7 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
+from WBC.wbc import WBC
 from WBC.utils import PilotError, notice
 from WBC.getparam import getparam
 
@@ -83,7 +86,7 @@ def checkset():
 # do some fancypants stuff to avoid having to type things multiple times.
 # one makes my fingers hurt, the other makes my brain hurt #PoisonChosen
 # beginning to appreciate cpp ...
-def _addparam1(optional, name, shortname, handler):
+def _addparam1(optional, name, shortname, handler, descr):
 	def x(arg):
 		try:
 			# reject special characters.  they should not be
@@ -102,6 +105,7 @@ def _addparam1(optional, name, shortname, handler):
 	param['name'] = name
 	param['shortname'] = shortname
 	param['optional'] = optional
+	param['descr'] = descr
 	paramparsers[name] = param
 
 	assert(shortname not in paramshorts)
@@ -140,33 +144,113 @@ paraminputs = {}		# longname  -> unparsed text input
 paramparsers = {}		# longname  -> param "struct"
 paramshorts = {}		# shortname -> longname
 
-_addparam('units_output',	'uo',	_currystring(['metric', 'us']))
-_addparam('strength_output',	'so',	_currystring(['plato','sg']))
+_addparam('units_output',	'uo',	_currystring(['metric', 'us']),
+				'Sets output units except for strength. '
+				'Acceptable values: [metric, us]')
+_addparam('strength_output',	'so',	_currystring(['plato','sg']),
+				'Sets output units for strength. '
+				'Acceptable values: [plato, sg]')
 
-_addparam('mash_efficiency',	'me',	parse.percent)
+_addparam('mash_efficiency',	'me',	parse.percent,
+				'The percentage of '
+				'extract from the mash making it onto '
+				'next stage.  If the wort you are collecting '
+				'is too weak or strong, adjust this parameter. '
+				'See also: mlt_loss. '
+				'Acceptable values: percentage. Typical '
+				'values: 70-90%')
 
-_addparam('boiloff_perhour',	'bo',	parse.volume)
+_addparam('boiloff_perhour',	'bo',	parse.volume,
+				'The volume-equivalent of room temperature '
+				'water that is boiled off per hour. '
+				'Acceptable values: volume. Typical values: '
+				'3.5L, 1gal')
 
-_addparam('mlt_loss',		'ml',	parse.volume)
-_addparam('mlt_heatcapacity',	'mh',	_parsefloat)
-_addparam('mlt_heat',		'mt',	_currystring(['transfer','direct']))
+_addparam('mlt_loss',		'ml',	parse.volume,
+				'The constant volume of wort that is lost in '
+				'the MLT deadspace.  If you are not collecting '
+				'the expected amount of wort from the sparge, '
+				'adjust this parameter and/or '
+				'grain_absorption.  See also: mash_efficiency. '
+				'Acceptable values: volume.')
+_addparam('mlt_heatcapacity',	'mh',	_parsefloat,
+				'The amount of heat capacity that the mashtun '
+				'has relative to a kilogram of water.  Adjust '
+				'this value up or down if measured mash '
+				'temperatures are too low or high, '
+				'respectively. Acceptable values: number. '
+				'Typical values: 1-2.')
 
-_addparam('grain_absorption',	'ga',	_curryratio(parse.volume, parse.mass))
+# XXX: it's not M*L*T heat
+_addparam('mlt_heat',		'mt',	_currystring(['transfer','direct']),
+				'Heat source for the mashtun.  Acceptable '
+				'values: [transfer, direct].  The former '
+				'relies on heated water or decoction '
+				'additions, the latter assumes no added water '
+				'for infusion mashes.')
 
-_addparam('kettle_loss',	'kl',	parse.volume)
-_addparam('fermentor_loss',	'fl',	parse.volume)
+_addparam('grain_absorption',	'ga',	_curryratio(parse.volume, parse.mass),
+				'The amount of liquid that grains hold. '
+				'Adjust the default down if you are e.g. '
+				'pressing the grains at the end of the mash. '
+				'This parameter takes the *true* absorption '
+				'of the grains instead of the difference '
+				'between water added and wort collected; the '
+				'former is constant and the latter depends '
+				'on wort strength. '
+				'Acceptable values: volume/mass (e.g. '
+				'"1.1L/kg").  See also: mlt_loss')
 
-_addparam('ambient_temp',	'Ta',	parse.temperature)
-_addparam('sparge_temp',	'Ts',	parse.temperature)
-_addparam('preboil_temp',	'Tp',	parse.temperature)
-_addparam('postboil_temp',	'Tb',	parse.temperature)
+_addparam('kettle_loss',	'kl',	parse.volume,
+				'The constant volume of room temperature wort '
+				'lost in the kettle.  Adjust this up or down '
+				'if your postboil volume matches the '
+				'prediction but your fermentor volume is off. '
+				'The total kettle loss is this value plus '
+				'the automatically calculated hop loss. '
+				'Acceptable values: volume')
+_addparam('fermentor_loss',	'fl',	parse.volume,
+				'The constant volume of room temperature wort '
+				'lost in the fermentor.  Adjust this up or '
+				'down if your fermentor volume matches the '
+				'prediction but your packaged volume is off. '
+				'Acceptable values: volume')
 
-_addparam('mashin_ratio',	'mr',	_parsemashinratio)
+_addparam('ambient_temp',	'Ta',	parse.temperature, 'TODO (maybe remove/rework?)')
 
-_addoptparam('mashwater_min',	'mm',	parse.volume)
-_addoptparam('mashvol_max',	'mM',	parse.volume)
-_addoptparam('lautervol_max',	'lM',	parse.volume)
-_addoptparam('boilvol_max',	'bM',	parse.volume)
+_addparam('sparge_temp',	'Ts',	parse.temperature,
+				'Temperature the sparge water volume is '
+				'reported for.  Acceptable values: temperature')
+_addparam('preboil_temp',	'Tp',	parse.temperature,
+				'Temperature the preboil volume is '
+				'reported for.  Acceptable values: temperature')
+_addparam('postboil_temp',	'Tb',	parse.temperature,
+				'Temperature the postboil volume is '
+				'reported for.  Acceptable values: temperature')
+
+_addparam('mashin_ratio',	'mr',	_parsemashinratio, 'TODO (rework what this means)')
+
+_addoptparam('mashwater_min',	'mm',	parse.volume, 'TODO (I do not remember why this parameter is necessary)')
+
+_addoptparam('mashvol_max',	'mM',	parse.volume,
+					'Maximum volume for the mash.  If the '
+					'limit is met, less water is used for '
+					'the mash and transferred to the '
+					'sparge. Overrides "mashin_ratio" in '
+					'case of a conflict. '
+					'Acceptable values: volume')
+_addoptparam('lautervol_max',	'lM',	parse.volume,
+					'Maximum volume for lauter. This '
+					'parameter assumes a single-step '
+					'lauter such as a "dunk sparge". '
+					'Acceptable values: volume')
+_addoptparam('boilvol_max',	'bM',	parse.volume,
+					'Maximum volume in the boil kettle. '
+					'If the limit is overrun, additional '
+					'water is reserved from the mash & '
+					'sparge, and automatically listed as '
+					'a fermentor-stage ingredient.'
+					'Acceptable values: volume')
 
 _defaults = {
 	# water absortion for 1kg of grain, gross (*true* absorption)
@@ -211,3 +295,9 @@ def decodeparamshorts(pstr):
 			raise PilotError('invalid sysparam spec: ' + x)
 		res.append((paramshorts[v[0]], v[1]))
 	return res
+
+if __name__ == '__main__':
+	for x in paramparsers:
+		p = paramparsers[x]
+		print(p['name'] + ': ' + p['descr'])
+		print()
